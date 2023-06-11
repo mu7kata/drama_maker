fc5fde0d75e7c86c95cfae61c949449840632a4e
m7kata <m7kata@icloud.com>
2023-06-11 20:35:31 +0900

    Vueの導入テスト

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {ref} from 'vue'
+
+const message = ref('Hello World!')
+console.log('test');
+</script>
+
+<template>
+    <div class="container">
+        <div class="header">AIドラマメーカー</div>
+        <a href="/home" class="link-start">スタート</a>
+    </div>
+    <h1>{{ message }}</h1>
+</template>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AIドラマメーカー</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         body {
             background: #e0e0e0;
@@ -41,9 +42,6 @@
     </style>
 </head>
 <body>
-<div class="container">
-    <div class="header">AIドラマメーカー</div>
-    <a href="/home" class="link-start">スタート</a>
-</div>
+<div id="app"></div>
 </body>
 </html>
