49f511b5367d237880bc0526038c46beed2803b8
m7kata <m7kata@icloud.com>
2023-07-05 18:29:54 +0900

    コメント修正

--- pr_action.sh
+++ pr_action.sh
@@ -6,6 +6,7 @@ echo ""
 # gh pr createの出力を変数に保存
 result_show_commit_log=$(gh pr create --base deployment/production --head main --title "Your PR title" --body "Your PR description")
 echo ""
+echo "result_show_commit_log"
 #if [[ -z "$result_show_commit_log" ]]; then
 #     echo "↑エラーログ"
 #     echo ""
@@ -14,13 +15,14 @@ echo ""
 #     exit 1
 #fi
 echo "成功しました"
-
-# output変数からPRのURLを抽出し、そのURLの最後の部分（PR番号）を取得
-pr_number=$(echo "$result_create_pr" | grep -o 'https://github.com/.*/pull/[0-9]*' | awk -F '/' '{print $NF}')
 echo ""
+
 echo "--------------------------------------------------------------------------------------------"
 echo "🔻 本番化対象の作業履歴(commit)です。意図しない差分が含まれていないか確認してください";
 echo "";
+
+# output変数からPRのURLを抽出し、そのURLの最後の部分（PR番号）を取得
+pr_number=$(echo "$result_create_pr" | grep -o 'https://github.com/.*/pull/[0-9]*' | awk -F '/' '{print $NF}')
 
 result_show_commit_log=$(gh pr view "$pr_number" --json commits --jq '.commits[] | "ユーザー：\(.authors[].name) | \(.messageHeadline) - "') >/dev/null 2>&1
 
@@ -33,7 +35,7 @@ if [[ -z "$result_show_commit_log" ]]; then
   exit 1
 fi
 echo "成功しました"
-
+echo ""
 
 echo "--------------------------------------------------------------------------------------------"
 echo 処理を選択してください
